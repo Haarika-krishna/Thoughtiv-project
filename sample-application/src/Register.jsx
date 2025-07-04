f43317c02e06a99969b040ade32c99d4d6ca83e4
HaarikaBoini <harika.boini.dev@gmail.com>
2025-07-04 20:18:37 +0530

    third commit

--- sample-application/src/Register.jsx
+++ sample-application/src/Register.jsx
@@ -25,7 +25,7 @@ const Register = ({ onSuccess }) => {
   }
 
   try {
-    const res = await fetch('http://localhost:5000/register', {
+    const res = await fetch('https://thoughtiv-project.onrender.com/register', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ ...form, device_id }),
