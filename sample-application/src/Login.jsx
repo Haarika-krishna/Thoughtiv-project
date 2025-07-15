feffdcc3eb0a60de44ad30840eaedf70fdfc9e77
HaarikaBoini <harika.boini.dev@gmail.com>
2025-07-15 12:15:44 +0530

    The lasttt

--- sample-application/src/Login.jsx
+++ sample-application/src/Login.jsx
@@ -17,7 +17,7 @@ const Login = ({ onSuccess }) => {
       }, {});
       const device_id = cookies.device_id;
 
-      const res = await fetch('http://localhost:5000/login', {
+      const res = await fetch('https://thoughtiv-project-1.onrender.com/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
